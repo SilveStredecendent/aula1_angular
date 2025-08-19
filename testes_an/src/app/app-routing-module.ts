c435a8e6218d7b5ea953b7684e7681ff89350694
Herbert de Sousa Santoss <117468625+SilveStredecendent@users.noreply.github.com>
2025-08-19 01:38:10 +0000

    Criação de módulos, componentes e rotas. Corrija a ortografia.

--- testes_an/src/app/app-routing-module.ts
+++ testes_an/src/app/app-routing-module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LivrosModule } from './livros/livros-module';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'livros', loadChildren: () => import('./livros/livros-module').then(m => m.LivrosModule) }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
