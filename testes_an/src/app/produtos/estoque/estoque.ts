051384a42ce249e633dac115aaa9a7f6f188ed10
Herbert de Sousa Santoss <117468625+SilveStredecendent@users.noreply.github.com>
2025-09-02 03:41:16 +0000

    feat: Finalizando o módulo produtos e o componente estoque

--- testes_an/src/app/produtos/estoque/estoque.ts
+++ testes_an/src/app/produtos/estoque/estoque.ts
@@ -16,13 +16,21 @@ export class Estoque {
   listaprodutos = [
     {nome: 'Nootebook', preco: 3500, estoque: 5},
     {nome: 'Teclado', preco: 150, estoque: 0},
-    {nome: 'Monitor', preco: 1200, estoque: 3}
+    {nome: 'Monitor', preco: 1200, estoque: 3},
+    {nome: 'Celular', preco: 5200, estoque: 0},
+    {nome: 'Smart Watch', preco: 1550, estoque: 4},
+    {nome: 'Mouse', preco: 200, estoque: 9}
   ];
 
-const valor = listaprodutos[0].preco; 
-
-  aumentarPreco()  {
-    this.aumentarPreco +=
+  aumentarPreco() {
+    this.listaprodutos.forEach(produto => {
+      produto.preco = parseFloat((produto.preco * 1.1).toFixed(2));
+    });
   }
 
+  diminuirPreco() {
+    this.listaprodutos.forEach(produto => {
+      produto.preco = parseFloat((produto.preco * 0.9).toFixed(2));
+    });
+  }
 }
