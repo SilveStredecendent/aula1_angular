fe5b044599ce46640ccc1deda1db8e40c39c409c
Herbert de Sousa Santoss <117468625+SilveStredecendent@users.noreply.github.com>
2025-09-09 01:37:16 +0000

    feat: Oraganizando a pagina inicial

--- testes_an/src/app/app-module.ts
+++ testes_an/src/app/app-module.ts
@@ -4,13 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+
 @NgModule({
   declarations: [
     App,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule, 
+    MatButtonModule, 
+    MatIconModule
   ],
   providers: [
     provideBrowserGlobalErrorListeners(),
